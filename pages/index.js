32fbf09ffb1c4f81b4ead1d099436570246c10b9
alasramus <77047027+alasramus@users.noreply.github.com>
2023-01-03 01:20:01 +0100

    Modified account, home grid, cleaned code

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,64 @@ export default function Home() {
   return (
     <>
       <body>
-      <p className="text-gray-700 text-3xl mb-16 font-semibold">Hello, Jonas!</p>
-      <div className="grid text-center lg:grid-cols-3 gap-8 mb-16">
-        <div className="rounded bg-white shadow-sm">nice1</div>
-        <div className="rounded bg-white h-40 shadow-sm">nice2</div>
-        <div className="rounded bg-white h-40 w-40 shadow-sm p-6">
-          <p className="pb-3">Level 1</p>
-          <div className="radial-progress bg-green-600 text-primary-content border-4 border-green-600" style={{"--value":25}}>25/200</div></div>
-      </div>
+        <p className="text-gray-700 text-3xl mb-16 font-semibold">Hello, Jonas!</p>
+        <div className="grid lg:grid-cols-3 gap-8 mb-16">
+          <div className="col-span-2 row-span-4 rounded bg-white shadow-sm">nice1</div>
+          <div className="rounded h-40 shadow-sm h-60 w-60">
+            <p>Missions</p>
+            <div class="mission-box">
+              <div class="flex items-center">
+                
+              <div class="ml-1">
+                <div class="text-sm font-semibold text-gray-600">Mission 1</div>
+                <div class="text-sm font-light text-gray-500">Aliquam tincidunt mauris eu risus.</div>
+              </div>
+              </div>
+              <div>
+                <button class="button-red">
+                  <svg class="text-white toggle__lock w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="mission-box">
+              <div class="flex items-center">
+                
+              <div class="ml-2">
+                <div class="text-sm font-semibold text-gray-600">Mission 1</div>
+                <div class="text-sm font-light text-gray-500">Aliquam tincidunt mauris eu risus.</div>
+              </div>
+              </div>
+              <div>
+                <button class="button-red">
+                  <svg class="text-white toggle__lock w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="mission-box">
+              <div class="flex items-center">
+                
+              <div class="ml-2">
+                <div class="text-sm font-semibold text-gray-600">Going There</div>
+                <div class="text-sm font-light text-gray-500">Complete module 2</div>
+              </div>
+              </div>
+              <div>
+                <button class="bg-red-500 hover:bg-red-400 p-2 rounded-full shadow-md flex justify-center items-center">
+                  <svg class="text-white toggle__lock w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="rounded text-center bg-white h-60 w-60 shadow-sm p-6 mt-10">
+            <h2 className="pb-3">Level 1</h2>
+            <div className="radial-progress bg-green-600 text-primary-content border-4 border-green-600" style={{"--value":25}}>25/200</div></div>
+        </div>
       </body>
       
     </>
